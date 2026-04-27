3612b0837a1dac917d0862588aa49bd3c244c3c9
CaiquePithon <caiquepithon2005@gmail.com>
2026-04-27 19:23:34 -0300

    feat: configurando dependencias

--- api/src/main/java/br/com/projeto/api/model/Professor.java
+++ api/src/main/java/br/com/projeto/api/model/Professor.java
@@ -1,9 +1,10 @@
 package br.com.projeto.api.model;
 
+import lombok.Data;
+
+  @Data
 public class Professor {
-
-
-    //Atributos
+    
     private String matricula;
     private String nome;
     private String email;
@@ -13,51 +14,4 @@ public class Professor {
     private int usuarioID;
     private int idade;
 
-    //Gets e Sets
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public int getIdade() {
-        return idade;
-    }
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-    public String getMatricula() {
-        return matricula;
-    }
-    public void setMatricula(String matricula) {
-        this.matricula = matricula;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public String getTelefone() {
-        return telefone;
-    }
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-    public int getEscolaID() {
-        return escolaID;
-    }
-    public void setEscolaID(int escolaID) {
-        this.escolaID = escolaID;
-    }
-    public int getUsuarioID() {
-        return usuarioID;
-    }
-    public void setUsuarioID(int usuarioID) {
-        this.usuarioID = usuarioID;
-    }
-    
-
-    
-    
 }
