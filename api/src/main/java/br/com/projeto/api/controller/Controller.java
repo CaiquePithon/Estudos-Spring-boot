d9df8d67c2045b9ffcf7ddd1efde11794ad19fc3
CaiquePithon <caiquepithon2005@gmail.com>
2026-04-28 16:21:18 -0300

    feat: Metodo de cadastramento de professores

--- api/src/main/java/br/com/projeto/api/controller/Controller.java
+++ api/src/main/java/br/com/projeto/api/controller/Controller.java
@@ -1,16 +1,25 @@
 package br.com.projeto.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import br.com.projeto.api.model.Professor;
+import br.com.projeto.api.repository.ProfessorRepository;
 
 @RestController
 public class Controller {
     
+    @Autowired
+    private ProfessorRepository acao;
+
+    @PostMapping("/cadastrarprofessor")
+    public Professor cadastrar(@RequestBody Professor p) {
+        return acao.save(p);
+    }
+
     @GetMapping("")
     public String mensagem() {
         return "Hello World!";
---
+++ api/src/main/java/br/com/projeto/api/repository/ProfessorRepository.java
@@ -0,0 +1,12 @@
+package br.com.projeto.api.repository;
+    
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import br.com.projeto.api.model.Professor;
+
+@Repository
+public interface ProfessorRepository extends CrudRepository<Professor, Integer> {
+
+
+    
+}
